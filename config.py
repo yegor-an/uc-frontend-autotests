5ec3a952a08380cab299806670a8d3535c06f0aa
Yegor <71543252+yegor-an@users.noreply.github.com>
2025-08-21 14:48:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-URL = os.getenv("BASE_URL")
+BASE_URL = os.getenv("BASE_URL")
 EMAIL = os.getenv("TEST_EMAIL_1")
 PASSWORD = os.getenv("TEST_PASSWORD")
 USERNAME = os.getenv("TEST_USERNAME") 
+
