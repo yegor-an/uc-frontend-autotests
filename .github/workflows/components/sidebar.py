c1b0ab3f11b40e00da5ecd81c98e0a7276cab078
Yegor <71543252+yegor-an@users.noreply.github.com>
2025-08-21 14:09:20 +0500

    Delete .github/workflows/components directory

--- .github/workflows/components/sidebar.py
+++
@@ -1,6 +0,0 @@
-from selene import browser, be, have, by
-
-class Sidebar:
-    def should_show_username(self, username):
-        browser.element(by.text(username)).should(be.visible).should(have.text(username))
-        return self
