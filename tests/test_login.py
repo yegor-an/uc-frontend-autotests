c33c3dd92cda78a4267c220a696e475e805156f6
Yegor <71543252+yegor-an@users.noreply.github.com>
2025-08-21 15:45:48 +0500

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,10 +12,10 @@ def test_login_with_enter():
         .sidebar.should_see_profile_with(USERNAME))
 
 
-@pytest.mark.qase(id=1)
-def test_login_with_button():
-    (LoginPage()
-        .open()
-        .login_with_button(EMAIL, PASSWORD)
-        .should_be_loaded()
-        .sidebar.should_see_profile_with(USERNAME))
+# @pytest.mark.qase(id=1)
+# def test_login_with_button():
+    # (LoginPage()
+        # .open()
+        # .login_with_button(EMAIL, PASSWORD)
+        # .should_be_loaded()
+        # .sidebar.should_see_profile_with(USERNAME))
