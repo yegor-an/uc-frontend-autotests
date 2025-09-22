5560cb8acf5b62d3c90b3d4bac9c2b17f1749296
yegor-an <yegor@helpless-balance.aeza.one>
2025-09-22 16:17:49 +0200

    minor

---
+++ bash/runner.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import subprocess
+import schedule
+import time
+
+# === НАСТРОЙКИ ===
+# Каждая строка: (ВРЕМЯ_В_ФОРМАТЕ_HH:MM, "имя_скрипта.sh")
+TASKS = [
+    ("16:10", "run_login_test.sh")
+]
+
+# === ЛОГИКА ===
+def run_script(script_name):
+    print(f"Запуск {script_name}")
+    subprocess.run(["bash", script_name], check=False)
+
+# Регистрируем расписание
+for t, script in TASKS:
+    schedule.every().day.at(t).do(run_script, script)
+
+print("Скрипт запущен. Ожидаем времени задач...")
+while True:
+    schedule.run_pending()
+    time.sleep(10)
